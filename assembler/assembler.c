2668ee8b3a104ff4dede03e7fbfaf2fbd59b308a
Tanay Soni <tanayvsoni@gmail.com>
2022-11-25 17:56:34 -0700

    8-Bit Computer

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -5,13 +5,12 @@
 
 int main()
 {
-    int *size = (int*)(malloc(sizeof(int)));
-    char **code = og_code(size);
-
     labels labels_list[MAX_LABELS];
     orgs orgs_list[MAX_ORGS];
     vars vars_list[MAX_VARS];
 
+    int *size = (int*)(malloc(sizeof(int)));
+    char **code = og_code(size);
     code = get_vars(vars_list, code, size);
 
     /*for (int i = 0; i < MAX_VARS; ++i)
